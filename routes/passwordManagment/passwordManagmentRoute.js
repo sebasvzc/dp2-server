df9187190e77aa1e4a72ebff626944037de99918
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2024-04-28 18:37:18 -0500

    apiCambioContraseniaWeb

--- routes/passwordManagment/passwordManagmentRoute.js
+++ routes/passwordManagment/passwordManagmentRoute.js
@@ -4,6 +4,8 @@ var passManagerController = require('../../controllers/passwordManagmentControll
 var passManagerRouter = express.Router();
 
 passManagerRouter.post('/olvidoPassword', passManagerController.forgotPassword);
-passManagerRouter.post('/cambiarPassword', passManagerController.changePassword)
+passManagerRouter.post('/cambiarPassword', passManagerController.changePassword);
+passManagerRouter.post('/olvidoPasswordWeb', passManagerController.forgotPasswordWeb);
+passManagerRouter.post('/cambiarPasswordWeb', passManagerController.changePasswordWeb);
 
 module.exports = passManagerRouter;
