4a121cb511f1402360346134db2f396fd973b63a
DiegoD <a20171086@pucp.edu.pe>
2024-05-21 22:25:57 -0500

    Comit

--- routes/tiendas/locatariosRoute.js
+++ routes/tiendas/locatariosRoute.js
@@ -2,9 +2,11 @@ const express = require('express');
 var tiendaController = require('../../controllers/tiendaController');
 const authenticateToken = require("../../middlewares/authenticateToken");
 
-const { getTiendas } = tiendaController
+const { getTiendas,deshabilitar, habilitar } = tiendaController
 var tiendaRouter = express.Router();
 
 tiendaRouter.get('/listartiendas', authenticateToken ,getTiendas);
+tiendaRouter.post('/deshabilitar', authenticateToken ,deshabilitar);
+tiendaRouter.post('/habilitar', authenticateToken ,habilitar);
 
 module.exports = tiendaRouter;
