7ef7fe77a6587699246d2433b60702e1aafe8a7c
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2024-06-06 19:26:50 -0500

    aaaa

--- scripts/proximosEventos.js
+++ scripts/proximosEventos.js
@@ -5,6 +5,11 @@ const { Op } = require("sequelize");
 const moment = require("moment");
 const { sendNotificationTodos } = require("../controllers/notificationsController");
 
+
+const { Expo } = require('expo-server-sdk');
+const expo = new Expo();
+
+
 const proximoEvento = async () => {
     try {
         const tomorrow = moment().add(1, 'days').startOf('day').toDate();
@@ -21,7 +26,51 @@ const proximoEvento = async () => {
         for (let evento of eventos) {
             const title = "Eventos de mañana";
             const body = `Evento: ${evento.nombre}, Fecha: ${moment(evento.fechaInicio).format('LLL')}`;
-            await sendNotificationTodos(title, body);
+            
+
+            try {
+                const userTokens = await db.notificationToken.findAll({ where: {activo: true } });
+                console.log(userTokens)
+        
+                if (!userTokens.length) {
+                    console.log('No tokens found for user');
+                    return 
+                }
+        
+                let messages = [];
+                for (let userToken of userTokens) {
+                    if (!Expo.isExpoPushToken(userToken.token)) {
+                        console.error(`Push token ${userToken.token} is not a valid Expo push token`);
+                        continue;
+                    }
+        
+                    messages.push({
+                        to: userToken.token,
+                        sound: 'default',
+                        title,
+                        body,
+                        data: { withSome: 'data' },
+                    });
+                }
+        
+                let chunks = expo.chunkPushNotifications(messages);
+                let tickets = [];
+                for (let chunk of chunks) {
+                    try {
+                        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                        tickets.push(...ticketChunk);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+        
+                console.log('Notification sent successfully');
+            } catch (error) {
+                console.error(error);
+                console.log('Error sending notification');
+            }
+
+
         }
     } catch (error) {
         console.error("Error al obtener los eventos de mañana:", error);
