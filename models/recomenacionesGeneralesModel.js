854475b5fb0020934c048f02c0252c05445c662a
Roberto Carlos Maldonado Morán <a20171016@pucp.edu.pe>
2024-06-13 14:23:37 -0500

    nueva tabla recomendaciones generales

---
+++ models/recomenacionesGeneralesModel.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const RecomendacionGeneral = sequelize.define("recomendacionGeneral", {
+        cuponFavorito: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'cupons', // nombre de la tabla referenciada
+                key: 'id'        // llave referenciada
+            }
+        },
+        cuponRecomendado: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'cupons', // nombre de la tabla referenciada
+                key: 'id'        // llave referenciada
+            }
+        },
+        prioridad: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        activo: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }, { timestamps: true });
+
+    return RecomendacionGeneral;
+}
